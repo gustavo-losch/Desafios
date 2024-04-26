1a0ab1838402ac65ca5d459cd7822611c0bb39a1
Gustavo Losch <gustavolosch@icloud.com>
2024-04-26 00:01:45 -0300

    Initial upload
    Upload inicial do Elixir Sagrado

---
+++ ClubeDeAlgoritmos/ElixirSagrado/ElixirSagrado.java
@@ -0,0 +1,54 @@
+package ClubeDeAlgoritmos.ElixirSagrado;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+public class ElixirSagrado {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner reader = new Scanner(new FileReader("C:\\Users\\Gustavo Losch\\Documents\\Repositórios\\Desafios\\ClubedeAlgoritmos\\ElixirSagrado\\input.txt"));
+        String[] vet = new String[146];
+        int ciclo, densidade, forcaS;
+        ciclo = 0;
+        forcaS = 0;
+        densidade = 1;
+    
+        for (int i = 0; reader.hasNextLine(); i++) {
+
+            ciclo += 1; 
+
+            forcaS = ciclo(ciclo, densidade, forcaS);
+
+            vet[i] = reader.nextLine();
+
+            if (vet[i].length() > 4) {
+                
+                ciclo += 1;
+                forcaS = ciclo(ciclo, densidade, forcaS);
+                
+                String num = "";
+                num = vet[i].substring(5);
+                densidade += Integer.parseInt(num);
+                
+            }
+        }
+
+        System.out.println("A força do sinal é: " + forcaS);
+        reader.close();
+    }
+
+    public static int ciclo(int ciclo, int densidade, int forcaS) {
+        int ciclob = ciclo + 20;
+        int sinal = forcaS;
+
+        if (ciclob % 40 == 0 && ciclo >= 20 && ciclo <= 220) {
+            System.out.println("A densidade no " + ciclo + "º ciclo é de " + densidade);
+            sinal += ciclo * densidade;
+        }
+
+        return sinal;
+    }
+}
+
+// addx 2 ciclos e muda densidade
+// noop 1 ciclo
