d31c079e0296248770916b4818c5b7c19f9c3008
Gustavo Losch <gustavolosch@icloud.com>
2024-04-28 21:22:14 -0300

    Create ArquivosReais.java

---
+++ ClubeDeAlgoritmos/ArquivosReais/ArquivosReais.java
@@ -0,0 +1,16 @@
+package ClubeDeAlgoritmos.ArquivosReais;
+
+/*
+ * Link do problema: "https://cdass.vercel.app/problemas/22"
+ */
+
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class ArquivosReais {
+    public static void main(String args[]) throws FileNotFoundException {
+        Scanner reader = new Scanner(new FileReader("C:\\Users\\User\\Desktop\\Gustavo\\Repositórios\\Desafios\\ClubeDeAlgoritmos\\ArquivosReais\\input.txt"));
+        
+    }
+}
